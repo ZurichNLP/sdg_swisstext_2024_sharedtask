48733fcdcf92e65e4c557945abc3974e77f8b982
Tobias Fankhauser <tobias7402@gmail.com>
2024-05-16 20:21:46 +0200

    update evaluation

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -22,6 +22,16 @@ class Evaluator:
             f.write(report)
         print(f"Report saved to {report_filename}")
 
+    def match_data_by_id(self, predictions, true_data):
+        true_data_dict = {item['ID']: item for item in true_data}
+        matched_predictions = []
+        matched_true_data = []
+        for pred in predictions:
+            if pred['ID'] in true_data_dict:
+                matched_predictions.append(pred)
+                matched_true_data.append(true_data_dict[pred['ID']])
+        return matched_predictions, matched_true_data
+
     def task_1_goldlabel(self, predictions, true_data):
         pred_labels = [item['SDG'] for item in predictions]
         true_labels = [item['SDG'] for item in true_data]
@@ -30,7 +40,6 @@ class Evaluator:
         return report
 
     def task_1_secondary(self, predictions, true_data):
-        # go over all predictions and look if the predicted label is part of the true_data['SDGS'], if so replace the main label with the predicted label
         for i, item in enumerate(predictions):
             if item['SDG'] in true_data[i]['SDGS']:
                 true_data[i]['SDG'] = item['SDG']
@@ -41,48 +50,40 @@ class Evaluator:
         return report
 
     def evaluate_task_1(self, predictions, true_data, prediction_file_path):
+        predictions, true_data = self.match_data_by_id(predictions, true_data)
         report = self.task_1_goldlabel(predictions, true_data)
         self.save_report(report, 1, prediction_file_path, "goldlabel")
         report = self.task_1_secondary(predictions, true_data)
         self.save_report(report, 1, prediction_file_path, "secondary")
 
-
-    def evaluate_task_2(self, predictions, true_data, prediction_file_path):
-        # Initialize LabelBinarizer and MultiLabelBinarizer
+    def evaluate_task_2_goldlabel(self, predictions, true_data):
         lb = LabelBinarizer()
-        mlb = MultiLabelBinarizer()
-
-        # Main Target Evaluation
         true_main_targets = lb.fit_transform([item['TARGET'] for item in true_data])
         pred_main_targets = lb.transform([item['TARGET'] for item in predictions])
         main_target_report = classification_report(true_main_targets, pred_main_targets, target_names=lb.classes_, zero_division=0)
-        print("Main Target Evaluation\n", main_target_report)
+        print("Task 2 Goldlabel - Main Target Evaluation\n", main_target_report)
+        return main_target_report
 
-        # Secondary Targets Evaluation
+    def evaluate_task_2_secondary(self, predictions, true_data):
+        mlb = MultiLabelBinarizer()
         true_secondary_targets = mlb.fit_transform([item['TARGETS'] for item in true_data])
         pred_secondary_targets = mlb.transform([item['TARGETS'] for item in predictions])
         secondary_target_report = classification_report(true_secondary_targets, pred_secondary_targets, target_names=mlb.classes_, zero_division=0)
-        print("Secondary Targets Evaluation\n", secondary_target_report)
+        print("Task 2 Secondary - Secondary Targets Evaluation\n", secondary_target_report)
+        return secondary_target_report
 
-        # Combined Evaluation
-        true_combined_targets = [(item['TARGET'],) + tuple(item['TARGETS']) for item in true_data]
-        pred_combined_targets = [(item['TARGET'],) + tuple(item['TARGETS']) for item in predictions]
-        true_combined_targets = mlb.fit_transform(true_combined_targets)
-        pred_combined_targets = mlb.transform(pred_combined_targets)
-        combined_report = classification_report(true_combined_targets, pred_combined_targets, target_names=mlb.classes_, zero_division=0)
-        print("Combined Evaluation\n", combined_report)
+    def evaluate_task_2(self, predictions, true_data, prediction_file_path):
+        predictions, true_data = self.match_data_by_id(predictions, true_data)
+        # Goldlabel Evaluation
+        main_target_report = self.evaluate_task_2_goldlabel(predictions, true_data)
+        self.save_report(main_target_report, 2, prediction_file_path, "goldlabel")
 
-        # Save combined report to file
-        self.save_report("\n".join(['##################\nMain Target Report',
-                                    main_target_report,
-                                    '##################\nSecondary Target Report',
-                                    secondary_target_report,
-                                    '##################\nCombined Report',
-                                    combined_report]), 2, prediction_file_path)
+        # Secondary Evaluation
+        secondary_target_report = self.evaluate_task_2_secondary(predictions, true_data)
+        self.save_report(secondary_target_report, 2, prediction_file_path, "secondary")
 
     def evaluate(self, task_number, prediction_file_path):
         original_data_path = f"{self.test_data_folder}/testdata_results.jsonl"
-
         predictions = self.load_data(prediction_file_path)
         true_data = self.load_data(original_data_path)
 
@@ -92,7 +93,6 @@ class Evaluator:
             self.evaluate_task_2(predictions, true_data, prediction_file_path)
         else:
             print("Invalid task number. Please specify 1 or 2.")
-
 
     def evaluate_all_participants(self):
         for folder in os.listdir(self.submission_folder):
@@ -107,7 +107,6 @@ class Evaluator:
                     task_number = int(full_task[-1])
                     self.evaluate(task_number, f"{self.submission_folder}/{folder}/{file}")
 
-
     def convert_to_jsonl(self, file, folder_path):
         with open(f"{folder_path}/{file}", 'r', encoding='utf-8') as f:
             data = json.load(f)
@@ -116,7 +115,6 @@ class Evaluator:
                 f.write(json.dumps(item) + '\n')
         file = os.path.splitext(file)[0] + '.jsonl'
         return file
-
 
 if __name__ == '__main__':
     evaluator = Evaluator()
