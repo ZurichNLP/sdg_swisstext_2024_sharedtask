45e6e9e94f024b6f4ad0b3242a806fd7472382f8
Tobias Fankhauser <tobias7402@gmail.com>
2024-05-16 19:21:54 +0200

    submission evaluation

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -17,17 +17,14 @@ class Evaluator:
     def save_report(self, report, task_number, prediction_file_path, evaluationtype=""):
         base_filename = os.path.basename(prediction_file_path)
         base_filename = os.path.splitext(base_filename)[0]
-        date_str = datetime.now().strftime("%Y-%m-%d")
-        # save the report in the same directory as the prediction file
-        save_dir = os.path.dirname(prediction_file_path)
-        report_filename = f"{save_dir}/{base_filename}_task{task_number}_report_{date_str}_{evaluationtype}.txt"
+        report_filename = f"../results/task{task_number}/{base_filename}_task{task_number}_report_{evaluationtype}.txt"
         with open(report_filename, 'w') as f:
             f.write(report)
         print(f"Report saved to {report_filename}")
 
     def task_1_goldlabel(self, predictions, true_data):
         pred_labels = [item['SDG'] for item in predictions]
-        true_labels = [item['MAIN SDG'] for item in true_data]
+        true_labels = [item['SDG'] for item in true_data]
         report = classification_report(true_labels, pred_labels, output_dict=False, zero_division=0)
         print("Task 1 Goldlabel Evaluation:\n", report)
         return report
@@ -36,9 +33,9 @@ class Evaluator:
         # go over all predictions and look if the predicted label is part of the true_data['SDGS'], if so replace the main label with the predicted label
         for i, item in enumerate(predictions):
             if item['SDG'] in true_data[i]['SDGS']:
-                true_data[i]['MAIN SDG'] = item['SDG']
+                true_data[i]['SDG'] = item['SDG']
         pred_labels = [item['SDG'] for item in predictions]
-        true_labels = [item['MAIN SDG'] for item in true_data]
+        true_labels = [item['SDG'] for item in true_data]
         report = classification_report(true_labels, pred_labels, output_dict=False, zero_division=0)
         print("Task 1 Secondary Evaluation:\n", report)
         return report
@@ -56,20 +53,20 @@ class Evaluator:
         mlb = MultiLabelBinarizer()
 
         # Main Target Evaluation
-        true_main_targets = lb.fit_transform([item['MAIN TARGET'] for item in true_data])
-        pred_main_targets = lb.transform([item['MAIN_TARGET'] for item in predictions])
+        true_main_targets = lb.fit_transform([item['TARGET'] for item in true_data])
+        pred_main_targets = lb.transform([item['TARGET'] for item in predictions])
         main_target_report = classification_report(true_main_targets, pred_main_targets, target_names=lb.classes_, zero_division=0)
         print("Main Target Evaluation\n", main_target_report)
 
         # Secondary Targets Evaluation
         true_secondary_targets = mlb.fit_transform([item['TARGETS'] for item in true_data])
-        pred_secondary_targets = mlb.transform([item['SECONDARY_TARGETS'] for item in predictions])
+        pred_secondary_targets = mlb.transform([item['TARGETS'] for item in predictions])
         secondary_target_report = classification_report(true_secondary_targets, pred_secondary_targets, target_names=mlb.classes_, zero_division=0)
         print("Secondary Targets Evaluation\n", secondary_target_report)
 
         # Combined Evaluation
-        true_combined_targets = [(item['MAIN_TARGET'],) + tuple(item['SECONDARY_TARGETS']) for item in true_data]
-        pred_combined_targets = [(item['MAIN_TARGET'],) + tuple(item['SECONDARY_TARGETS']) for item in predictions]
+        true_combined_targets = [(item['TARGET'],) + tuple(item['TARGETS']) for item in true_data]
+        pred_combined_targets = [(item['TARGET'],) + tuple(item['TARGETS']) for item in predictions]
         true_combined_targets = mlb.fit_transform(true_combined_targets)
         pred_combined_targets = mlb.transform(pred_combined_targets)
         combined_report = classification_report(true_combined_targets, pred_combined_targets, target_names=mlb.classes_, zero_division=0)
@@ -84,7 +81,7 @@ class Evaluator:
                                     combined_report]), 2, prediction_file_path)
 
     def evaluate(self, task_number, prediction_file_path):
-        original_data_path = f"{self.test_data_folder}/testdata.jsonl"
+        original_data_path = f"{self.test_data_folder}/testdata_results.jsonl"
 
         predictions = self.load_data(prediction_file_path)
         true_data = self.load_data(original_data_path)
